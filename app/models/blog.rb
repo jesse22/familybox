f1788a4c3c8e255549237a0130fd695001a8dbc8
Jesse <newman.jesse@gmail.com>
2014-05-01 10:15:26 +0000

    added upload image capability

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,5 +5,7 @@ class Blog < ActiveRecord::Base
   validates :user_id, presence: true
   
   validates :post, length: { minimum: 10 }, presence: true
-  
+  #For adding image to blog post
+  #attr_accessible :image
+  mount_uploader :image, ImageUploader
 end
